50127a930079619b273e42177ba20a0f63e03383
Alex Kenny <alexkenny1@gmail.com>
2018-10-04 15:15:30 +0000

    Mod html and js before refactoring

--- js/javascript.js
+++ js/javascript.js
@@ -1,71 +1,74 @@
 let lives;
 let livesbox = document.getElementById("lives");
 
-let answerbox = document.getElementById("gameButtons");
+// let answerbox = document.getElementById("gameButtons");
 
-let gamebox1 = document.getElementById("room1Game");
-let gamebox2 = document.getElementById("room2Game");
+let currentRoom;
 
 let rightAnswerArray = [];
 let answerArray = [];
 
-function setRoom1Game() {
-    answerbox.setAttribute("data-roomNumber", "room1");
-    escapeRoom1();
-}
+let checkComplete1;
+let checkComplete2;
 
-function setRoom2Game() {
-    answerbox.setAttribute("data-roomNumber", "room2");
-    escapeRoom2();
-}
-
-
-let roomNumber = answerbox.getAttribute("data-roomNumber");
 
 $(".button").on("click", function() {
     answerArray.push($(this).attr("data-btnNum"));
-    console.log(answerArray.length);
+    // console.log(answerArray.length);
     console.log(answerArray);
 
     for (let i = 0; i < answerArray.length; i++) {
         if (answerArray[i] == rightAnswerArray[i]) {
             // console.log("correct");
             if (answerArray[5] == rightAnswerArray[5]) {
-                console.log("You have escaped");
-            }
-            else {
-                console.log("Correct");
+                console.log("You've escaped");
+                if (currentRoom == "room1") {
+                    escapedRoom1();
+                }
+                else if (currentRoom == "room2") {
+                    escapedRoom2();
+                }
             }
         }
         else {
-            console.log("Incorrect");
+            // console.log("Incorrect");
             answerArray = [];
             lives--;
         }
+    }
+    
+    if (checkComplete1 == true && checkComplete2 == true) {
+        console.log("congratulations");
+        escapedBoth();            
     }
 
     livesbox.textContent = lives;
 
     if (lives == 0) {
-        console.log("You did not escape, you are stuck in here forever")
+        // console.log("You did not escape, you are stuck in here forever")
+        notEscaped();
     }
 
 });
 
 
 function escapeRoom1() {
+    currentRoom = "room1";
+
     gameDisplay()
-    
+
     rightAnswerArray = ["btnNum3", "btnNum2", "btnNum4", "btnNum1", "btnNum6", "btnNum5"];
     answerArray = [];
-    
+
     lives = 3;
     livesbox.textContent = lives;
-    
-    gamebox1;
+
+    gameContentRoom1()
 }
 
 function escapeRoom2() {
+    currentRoom = "room2";
+
     gameDisplay()
 
     rightAnswerArray = ["btnNum4", "btnNum1", "btnNum3", "btnNum5", "btnNum2", "btnNum6"];
@@ -74,23 +77,99 @@ function escapeRoom2() {
     lives = 3;
     livesbox.textContent = lives;
 
-    gamebox2;
+    gameContentRoom2()
 }
 
 function gameStart() {
-    document.getElementById("gameIntro").style.display="block";
-    document.getElementById("gameSelection").style.display="block";
-    document.getElementById("gameLives").style.display="none";
-    document.getElementById("gameButtons").style.display="none";
-    document.getElementById("room1Game").style.display="none";
-    document.getElementById("room2Game").style.display="none";
+    document.getElementById("gameIntro").style.display = "block";
+    document.getElementById("gameSelectionRoom1").style.display = "inline-block";
+    document.getElementById("gameSelectionRoom2").style.display = "inline-block";
+    document.getElementById("gameLives").style.display = "none";
+    document.getElementById("gameButtons").style.display = "none";
+    document.getElementById("gameRoom1").style.display = "none";
+    document.getElementById("gameRoom2").style.display = "none";
+    document.getElementById("escapedRoom1").style.display = "none";
+    document.getElementById("escapedRoom2").style.display = "none";
+    document.getElementById("notEscaped").style.display = "none";
+    document.getElementById("escapedBoth").style.display = "none";
 }
 
 function gameDisplay() {
-    document.getElementById("gameIntro").style.display="none";
-    document.getElementById("gameSelection").style.display="block";
-    document.getElementById("gameLives").style.display="block";
-    document.getElementById("gameButtons").style.display="block";
+    document.getElementById("gameIntro").style.display = "none";
+    document.getElementById("gameLives").style.display = "block";
+    document.getElementById("gameButtons").style.display = "block";
+    document.getElementById("escapedRoom1").style.display = "none";
+    document.getElementById("escapedRoom2").style.display = "none";
+    document.getElementById("notEscaped").style.display = "none";
+    document.getElementById("escapedBoth").style.display = "none";
+}
+
+function gameContentRoom1() {
+    document.getElementById("gameSelectionRoom1").style.display = "none";
+    document.getElementById("gameSelectionRoom2").style.display = "block";
+    document.getElementById("gameRoom1").style.display = "block";
+    document.getElementById("gameRoom2").style.display = "none";
+}
+
+function gameContentRoom2() {
+    document.getElementById("gameSelectionRoom1").style.display = "block";
+    document.getElementById("gameSelectionRoom2").style.display = "none"
+    document.getElementById("gameRoom1").style.display = "none";
+    document.getElementById("gameRoom2").style.display = "block";
+}
+
+function escapedRoom1() {
+    document.getElementById("gameSelectionRoom1").style.display = "none";
+    document.getElementById("gameSelectionRoom2").style.display = "block";
+    document.getElementById("gameLives").style.display = "none";
+    document.getElementById("gameButtons").style.display = "none";
+    document.getElementById("gameRoom1").style.display = "none";
+    document.getElementById("gameRoom2").style.display = "none";
+    document.getElementById("escapedRoom1").style.display = "block";
+    document.getElementById("notEscaped").style.display = "none";
+    document.getElementById("escapedBoth").style.display = "none";
+    
+    checkComplete1 = true;
+}
+
+function escapedRoom2() {
+    document.getElementById("gameSelectionRoom1").style.display = "block";
+    document.getElementById("gameSelectionRoom2").style.display = "none";
+    document.getElementById("gameLives").style.display = "none";
+    document.getElementById("gameButtons").style.display = "none";
+    document.getElementById("gameRoom1").style.display = "none";
+    document.getElementById("gameRoom2").style.display = "none";
+    document.getElementById("escapedRoom2").style.display = "block";
+    document.getElementById("notEscaped").style.display = "none";
+    document.getElementById("escapedBoth").style.display = "none";
+    
+    checkComplete2 = true;
+}
+
+function notEscaped() {
+    document.getElementById("gameSelectionRoom1").style.display = "inline-block";
+    document.getElementById("gameSelectionRoom2").style.display = "inline-block";
+    document.getElementById("gameLives").style.display = "none";
+    document.getElementById("gameButtons").style.display = "none";
+    document.getElementById("gameRoom1").style.display = "none";
+    document.getElementById("gameRoom2").style.display = "none";
+    document.getElementById("escapedRoom1").style.display = "none";
+    document.getElementById("escapedRoom2").style.display = "none";
+    document.getElementById("notEscaped").style.display = "block";
+    document.getElementById("escapedBoth").style.display = "none";
+}
+
+function escapedBoth() {
+    document.getElementById("gameSelectionRoom1").style.display = "none";
+    document.getElementById("gameSelectionRoom2").style.display = "none";
+    document.getElementById("gameLives").style.display = "none";
+    document.getElementById("gameButtons").style.display = "none";
+    document.getElementById("gameRoom1").style.display = "none";
+    document.getElementById("gameRoom2").style.display = "none";
+    document.getElementById("escapedRoom1").style.display = "none";
+    document.getElementById("escapedRoom2").style.display = "none";
+    document.getElementById("notEscaped").style.display = "none";
+    document.getElementById("escapedBoth").style.display = "block";
 }
 
 gameStart();
